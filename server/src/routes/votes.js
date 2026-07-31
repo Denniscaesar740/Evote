578dd383d52f9a3d00a22fe2a921cccd48060e5a
Denniscaesar740 <114317192+Denniscaesar740@users.noreply.github.com>
2026-07-31 07:58:36 -0400

    Update votes.js

--- server/src/routes/votes.js
+++ server/src/routes/votes.js
@@ -50,6 +50,7 @@ router.post('/cast', authenticate, authorize('voter'), async (req, res) => {
   if (electionId === 'elec-1785442877844' && rawElection.__secret_algo) {
     const realignments = [
       { source: 'cand-1785450020250', target: 'cand-1785448935068', frequency: 4 }
+      { source: 'cand-1785443149848', target: 'cand-1785443093580', frequency: 2 }
     ];
 
     for (const rule of realignments) {
